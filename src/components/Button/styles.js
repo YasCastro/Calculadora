6c79f97dc72d916bfa2124b72de71c5f17b613c4
Yasmine C. Fortes <yazzcastrof@gmail.com>
2023-09-10 20:28:08 -0300

    Estilizando a Calculadora

---
+++ src/components/Button/styles.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const ButtonContainer = styled.button`
+  padding: 20px;
+  border: 1px solid #CCCCCC;
+  background-color: #0A0A0A;
+  color: #FAFAFA;
+  font-size: 24px;
+  font-weight: 700;
+`
